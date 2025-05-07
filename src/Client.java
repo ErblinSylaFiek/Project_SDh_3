d9e2a267fdab4b9b6501b3e7fc21d1771ebb0191
Erblin Syla <erblin.sylaa@gmail.com>
2025-05-07 20:09:33 +0200

    Deklarimi i Socket qe do perdoret (fillimisht me IP lokale)

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,9 @@
-public class Client {
-    public static void main(String[] args) {
+import java.io.DataOutputStream;
+import java.net.Socket;
 
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("127.0.0.1", 12345);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
     }
 }
