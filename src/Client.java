cf45c5edfcf5882a163bc0d9b4809cab2c42d114
Erblin Syla <erblin.sylaa@gmail.com>
2025-05-09 10:07:52 +0200

    Gjenerimi i qelesit DES

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,5 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.io.DataOutputStream;
 import java.net.Socket;
 
@@ -5,5 +7,8 @@ public class Client {
     public static void main(String[] args) throws Exception {
         Socket socket = new Socket("127.0.0.1", 12345);
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+        SecretKey desKey = keyGen.generateKey();
     }
 }
