d0652c7fc115d019764e0f8cb8fb46fe165f763f
Erblin Syla <erblin.sylaa@gmail.com>
2025-05-12 14:59:55 +0200

    Gjenerimi i IV (Initialization Vector)

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,10 @@
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.security.SecureRandom;
 
 public class Client {
     public static void main(String[] args) throws Exception {
@@ -14,5 +16,13 @@ public class Client {
 
         String message = "Pershendetje nga klienti";
         Cipher desCipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+
+        SecureRandom random = new SecureRandom();
+        byte[] iv = new byte[8];
+        random.nextBytes(iv);
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+        desCipher.init(Cipher.ENCRYPT_MODE, desKey, ivSpec);
+        byte[] encryptedMessage = desCipher.doFinal(message.getBytes());
     }
 }
