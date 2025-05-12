d5e199c0769b391660a8fa06bde3e66bcd64ec8a
eraberisha <era.berisha5@student.uni-pr.edu>
2025-05-12 22:50:57 +0200

    Shtimi i metodes createServer

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,14 @@
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.IOException;
 
 public class Server {
+    public static ServerSocket createServer()throws IOException{
+        String serverIp = "127.0.0.1";
+        int serverPort = 12345;
+        ServerSocket serverSocket = new ServerSocket(serverPort);
+        System.out.println("Serveri po pret lidhje...");
+        return serverSocket;
+    }
     public static void main(String [] args){
 
     }
