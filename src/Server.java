45f445d1eea9a1be07c63ce042bfdbb6d20e1644
eraberisha <era.berisha5@student.uni-pr.edu>
2025-05-14 14:55:31 +0200

    Funksioni per ngarkimin e celesit privat te serverit

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,14 @@
 import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 public class Server{
-    public static void main(String []args){
-        try{
+    public static void main(String []args)throws IOException {
             ServerSocket serverSocket=new ServerSocket(12345);
             System.out.println("Serveri eshte duke pritur lidhje...");
 
@@ -24,12 +28,16 @@ public class Server{
             byte[] iv = new byte[8];
             in.readFully(iv);
 
-            //...
-
             clientSocket.close();
             serverSocket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    }
+    private static PrivateKey loadPrivateKey(String filePath) throws Exception {
+        FileInputStream keyFile = new FileInputStream(filePath);
+        byte[] keyBytes = keyFile.readAllBytes();
+        keyFile.close();
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(keySpec);
     }
 }
