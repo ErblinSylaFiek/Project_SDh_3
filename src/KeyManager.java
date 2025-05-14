f6647553e614f0275e1858becd6744fdb5401ac7
Ereza Greicevci <ereza.greicevci@student.uni-pr.edu>
2025-05-14 15:15:14 +0200

    Importimi i disa librarive te nevojshme, Krijimi i çelësave RSA dhe RSA 2048-bit

--- src/KeyManager.java
+++ src/KeyManager.java
@@ -1,2 +1,16 @@
+import java.io.*;
+import java.security.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.util.Base64;
+
 public class KeyManager {
+
+    // Krijimi i çelësave RSA
+    public static void generateRsaKeys() throws Exception {
+        // Krijimi i çelësave RSA 2048-bit
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+    }
 }
