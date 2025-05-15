36ebdc19c709c3b4966079cbf6ca778ce32c8d65
Ereza Greicevci <ereza.greicevci@student.uni-pr.edu>
2025-05-15 23:32:25 +0200

    Krijimi i nje celesi sekret per DES-CBC

--- src/KeyManager.java
+++ src/KeyManager.java
@@ -13,4 +13,30 @@ public class KeyManager {
         keyPairGenerator.initialize(2048);
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
     }
+
+    // Ekspozimi i çelësave
+    PrivateKey privateKey = keyPair.getPrivate();
+    PublicKey publicKey = keyPair.getPublic();
+
+    // Ruajtja e çelësave në skedarë
+        try(
+    FileOutputStream privateKeyFile = new FileOutputStream("server_private.pem");
+    FileOutputStream publicKeyFile = new FileOutputStream("server_public.pem"))
+
+    {
+        privateKeyFile.write(privateKey.getEncoded());
+        publicKeyFile.write(publicKey.getEncoded());
+    }
+
+        System.out.println("Çelësat RSA janë krijuar dhe ruajtur.");
+
+    // Krijimi i një çelësi sekret për DES-CBC
+    public static SecretKey generateSecretKey() throws Exception {
+        // Krijimi i çelësit sekret 8-byte për DES
+        byte[] keyBytes = new byte[8]; // DES kërkon një çelës 8-byte
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(keyBytes);
+        SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "DES");
+        return secretKey;
+    }
 }
