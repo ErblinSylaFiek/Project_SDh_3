593595871ee1a4bdb12a7e075b07cedff401b13c
Ereza Greicevci <ereza.greicevci@student.uni-pr.edu>
2025-05-15 23:46:14 +0200

    Testimi i krijimit te celesave

--- src/KeyManager.java
+++ src/KeyManager.java
@@ -12,31 +12,43 @@ public class KeyManager {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(2048);
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
-    }
 
-    // Ekspozimi i çelësave
-    PrivateKey privateKey = keyPair.getPrivate();
-    PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
 
-    // Ruajtja e çelësave në skedarë
-        try(
-    FileOutputStream privateKeyFile = new FileOutputStream("server_private.pem");
-    FileOutputStream publicKeyFile = new FileOutputStream("server_public.pem"))
-
-    {
-        privateKeyFile.write(privateKey.getEncoded());
-        publicKeyFile.write(publicKey.getEncoded());
-    }
+        // Ruajtja e çelësave në skedarë
+        try (
+                FileOutputStream privateKeyFile = new FileOutputStream("server_private.pem");
+                FileOutputStream publicKeyFile = new FileOutputStream("server_public.pem")
+        ) {
+            privateKeyFile.write(privateKey.getEncoded());
+            publicKeyFile.write(publicKey.getEncoded());
+        }
 
         System.out.println("Çelësat RSA janë krijuar dhe ruajtur.");
+    }
 
     // Krijimi i një çelësi sekret për DES-CBC
     public static SecretKey generateSecretKey() throws Exception {
-        // Krijimi i çelësit sekret 8-byte për DES
         byte[] keyBytes = new byte[8]; // DES kërkon një çelës 8-byte
         SecureRandom random = new SecureRandom();
         random.nextBytes(keyBytes);
         SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "DES");
         return secretKey;
     }
+
+    // Dërgo çelësin publik te klienti (në këtë rast vetëm leximi i çelësit)
+    public static byte[] sendPublicKey() throws IOException {
+        try (FileInputStream publicKeyFile = new FileInputStream("server_public.pem")) {
+            return publicKeyFile.readAllBytes(); // Java 9+ ose përdor .available() me Java 8
+        }
+    }
+
+    // Testimi i krijimit të çelësave
+    public static void testKeyCreation() throws Exception {
+        generateRsaKeys();
+        SecretKey secretKey = generateSecretKey();
+        System.out.println("Çelësi sekret (hex): " + bytesToHex(secretKey.getEncoded()));
+    }
+
 }
