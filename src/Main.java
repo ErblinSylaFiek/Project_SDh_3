1a80a642b1faf2cf4076f82026a02bb14a18cb01
erikbehrami <erik.behrami@student.uni-pr.edu>
2025-05-16 16:17:11 +0200

    Implementimi i metodës Main për testim

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+
+        Thread serverThread = new Thread(() -> {
+            try {
+                Server.main(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        serverThread.start();
+
+        Thread.sleep(1000);
+
+        Client.main(null);
     }
 }
